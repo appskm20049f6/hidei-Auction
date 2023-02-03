4184f2e08915b833297fc9a4b5d6302f682a1e68
Koi tw <appskm20049f6@gmail.com>
2023-02-03 13:44:54 +0800

    Update page1.vue
    iphone 手機時間格式調整

--- src/components/page1.vue
+++ src/components/page1.vue
@@ -76,7 +76,7 @@ let pricepushtop = (e) => {
   }
 };
 
-const endtime = new Date("2023 / 02 / 05 15:30");
+const endtime = new Date("2023-02-05 16:00");
 let nowTime = new Date();
 
 let overLine = ref((endtime - nowTime) / 1000);
